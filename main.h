2b196b677dd2d210579de3b532ba310485e6f2a8
nouhaila297 <Nouhaila.zouine.1@gmail.com>
2023-06-25 12:43:11 -0700

    main.h file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+int _putch(char c);
+int print_string(char *s);
+int len(int num);
+int print_int(int n);
+int _printf(const char *format, ...);
+#endif /* MAIN_H */
+
